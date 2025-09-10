b76a0f03516f8b3d4648a624a1f349bccfb98ebf
Mohammad Irfan <irfan.noorzada123@gmail.com>
2025-09-10 19:32:28 +0430

    new changes

---
+++ js/lang.js
@@ -0,0 +1,19 @@
+/* const translations = {
+  en: {
+    home_title:
+    about_title:
+    about_text: 
+    contact_title:
+    hire_me_btn:
+    download_cv_btn: 
+  },
+  fa: {
+    home_title:
+    about_title:
+    about_text: 
+    contact_title:
+    hire_me_btn:
+    download_cv_btn: 
+  },
+};
+ */
